2b9cf9f0ebbac01921e55a1f02180a7073a02af4
Павел Левицкий <creative-creative@outlook.com>
2022-12-10 11:49:39 +0700

    Добавление структуры условий

--- Program.cs
+++ Program.cs
@@ -58,117 +58,117 @@ namespace Expert_system_for_choosing_antivirus_software
 
             bool result;
 
-            Console.WriteLine("Далее Вам будут заданы вопросы для подбора соответствующего Вам антивирусного пакета. Ответьте на них, используя 10-балльную шкалу (вводите цифры от 0 до 10).\r\n");
+            Console.WriteLine("Далее Вам будут заданы вопросы для подбора соответствующего Вам антивирусного пакета. Ответьте на них, используя цифры 1 или 2 (ДА или НЕТ соответственно)\r\n");
             
-            Console.WriteLine("Насколько Вам важна надежность антивирусного пакета?");
+            Console.WriteLine("Вам важна надежность антивирусного пакета?");
             do
             {
                 result = int.TryParse(Console.ReadLine(), out security);
-                if (result || security < 0 || security > 10)
-                {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
-                }
-            } while (!result || security < 0 || security > 10);
-            Console.WriteLine(security);
+            } while (!result || security < 1 || security > 2);
+            Console.WriteLine(security);//first decision
 
-            Console.WriteLine("Насколько Вам важно удобство использования антивирусного пакета?");
-            do
+            if (security == 1)//vetka 1
             {
-                result = int.TryParse(Console.ReadLine(), out ease_of_use);
-                if (result || ease_of_use < 0 || ease_of_use > 10)
+                Console.WriteLine("Важно ли удобство использования антивирусного пакета");
+                do
                 {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
-                }
-            } while (!result || ease_of_use < 0 || ease_of_use > 10);
-            Console.WriteLine(ease_of_use);
+                    result = int.TryParse(Console.ReadLine(), out ease_of_use);
+                } while (!result || ease_of_use < 1 || ease_of_use > 2);
+                Console.WriteLine(ease_of_use);
+                
+                if (ease_of_use == 1)
+                Console.WriteLine("Вам  важна ");
+            }
 
-            Console.WriteLine("Насколько Вам важно наличие комплексной защиты?");
-            do
-            {
-                result = int.TryParse(Console.ReadLine(), out comprehensive_protection_availability);
-                if (result || comprehensive_protection_availability < 0 || comprehensive_protection_availability > 10)
-                {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
-                }
-            } while (!result || comprehensive_protection_availability < 0 || comprehensive_protection_availability > 10);
-            Console.WriteLine(comprehensive_protection_availability);
 
-            Console.WriteLine("Насколько Вам важно качество защиты?");
-            do
+            else if (security == 2)//vetka 2
             {
-                result = int.TryParse(Console.ReadLine(), out protection_quality);
-                if (result || protection_quality < 0 || protection_quality > 10)
+                Console.WriteLine("Вам важно наличие комплексной защиты?");
+                do
                 {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
+                    result = int.TryParse(Console.ReadLine(), out comprehensive_protection_availability);
+                } while (!result || comprehensive_protection_availability < 1 || comprehensive_protection_availability > 2);
+                Console.WriteLine(comprehensive_protection_availability);
+                if(comprehensive_protection_availability == 1)
+                {
+                    Console.WriteLine(comprehensive_protection_availability);
                 }
-            } while (!result || protection_quality < 0 || protection_quality > 10);
-            Console.WriteLine(protection_quality);
+            }
 
-            Console.WriteLine("Насколько Вам важна высокая эффективность обнаружения угроз?");
-            do
-            {
-                result = int.TryParse(Console.ReadLine(), out efficiency_of_threat_detection);
-                if (result || efficiency_of_threat_detection < 0 || efficiency_of_threat_detection > 10)
-                {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
-                }
-            } while (!result || efficiency_of_threat_detection < 0 || efficiency_of_threat_detection > 10);
-            Console.WriteLine(efficiency_of_threat_detection);
 
-            Console.WriteLine("Насколько Вам важен обьем занимаемой антивирусным пакетом памяти?");
-            do
-            {
-                result = int.TryParse(Console.ReadLine(), out memory_footprint);
-                if (result || memory_footprint < 0 || memory_footprint > 10)
-                {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
-                }
-            } while (!result || memory_footprint < 0 || memory_footprint > 10);
-            Console.WriteLine(memory_footprint);
 
-            Console.WriteLine("Насколько Вам важна высокая частота получения обновлений?");
-            do
-            {
-                result = int.TryParse(Console.ReadLine(), out update_frequency);
-                if (result || update_frequency < 0 || update_frequency > 10)
-                {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
-                }
-            } while (!result || update_frequency < 0 || update_frequency > 10);
-            Console.WriteLine(update_frequency);
 
-            Console.WriteLine("Насколько Вам важна возможность вылечить зараженый компьютер?");
-            do
-            {
-                result = int.TryParse(Console.ReadLine(), out infected_computer_curing);
-                if (result || infected_computer_curing < 0 || infected_computer_curing > 10)
-                {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
-                }
-            } while (!result || infected_computer_curing < 0 || infected_computer_curing > 10);
-            Console.WriteLine(infected_computer_curing);
 
-            Console.WriteLine("Насколько Вам важно низкое влияние на производительность?");
-            do
-            {
-                result = int.TryParse(Console.ReadLine(), out performance_impact);
-                if (result || performance_impact < 0 || performance_impact > 10)
-                {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
-                }
-            } while (!result || performance_impact < 0 || performance_impact > 10);
-            Console.WriteLine(performance_impact);
 
-            Console.WriteLine("Насколько Вам важна высокая периодичность обновления вирусных баз?");
-            do
-            {
-                result = int.TryParse(Console.ReadLine(), out virus_database_updates_frequency);
-                if (result || virus_database_updates_frequency < 0 || virus_database_updates_frequency > 10)
-                {
-                    Console.WriteLine("Введите Вашу оценку по 10-балльной шкале (от 0 до 10)");
-                }
-            } while (!result || virus_database_updates_frequency < 0 || virus_database_updates_frequency > 10);
-            Console.WriteLine(virus_database_updates_frequency);
+
+
+
+
+
+
+
+            //Console.WriteLine("Вам важно удобство использования антивирусного пакета?");
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out ease_of_use);
+            //} while (!result || ease_of_use < 1 || ease_of_use > 2);
+            //Console.WriteLine(ease_of_use);
+
+            //Console.WriteLine("Вам важно наличие комплексной защиты?");
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out comprehensive_protection_availability);
+            //} while (!result || comprehensive_protection_availability < 1 || comprehensive_protection_availability > 2);
+            //Console.WriteLine(comprehensive_protection_availability);
+
+            //Console.WriteLine("Насколько Вам важно качество защиты?");
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out protection_quality);
+            //} while (!result || protection_quality < 1 || protection_quality > 2);
+            //Console.WriteLine(protection_quality);
+
+            //Console.WriteLine("Насколько Вам важна высокая эффективность обнаружения угроз?");
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out efficiency_of_threat_detection);
+            //} while (!result || efficiency_of_threat_detection < 1 || efficiency_of_threat_detection > 2);
+            //Console.WriteLine(efficiency_of_threat_detection);
+
+            //Console.WriteLine("Насколько Вам важен обьем занимаемой антивирусным пакетом памяти?");
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out memory_footprint);
+            //} while (!result || memory_footprint < 1 || memory_footprint > 2);
+            //Console.WriteLine(memory_footprint);
+
+            //Console.WriteLine("Насколько Вам важна высокая частота получения обновлений?");
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out update_frequency);
+            //} while (!result || update_frequency < 1 || update_frequency > 2);
+            //Console.WriteLine(update_frequency);
+
+            //Console.WriteLine("Насколько Вам важна возможность вылечить зараженый компьютер?");
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out infected_computer_curing);
+            //} while (!result || infected_computer_curing < 1 || infected_computer_curing > 2);
+            //Console.WriteLine(infected_computer_curing);
+
+            //Console.WriteLine("Насколько Вам важно низкое влияние на производительность?");
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out performance_impact);
+            //} while (!result || performance_impact < 1 || performance_impact > 2);
+            //Console.WriteLine(performance_impact);
+
+            //Console.WriteLine("Насколько Вам важна высокая периодичность обновления вирусных баз?");
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out virus_database_updates_frequency);
+            //} while (!result || virus_database_updates_frequency < 1 || virus_database_updates_frequency > 2);
+            //Console.WriteLine(virus_database_updates_frequency);
 
 
 
