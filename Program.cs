b99beb6b24f5e20c0331740c6ddbc5bbf0fea52b
Павел Левицкий <creative-creative@outlook.com>
2022-12-11 16:23:41 +0700

    Добавлены условия выбора, карта решений, диаграмма древа решений, обновлена "база знаний", подправлен вывод

--- Program.cs
+++ Program.cs
@@ -9,8 +9,9 @@ namespace Expert_system_for_choosing_antivirus_software
     {
         static void Main(string[] args)
         {
-            List<string> namesOfSoft = new()
+            List<string> knowledgeBase = new()
             {
+                "[zero_index]",
                 "Acronis(Static ML)",
                 "Ad-Aware",
                 "AhnLab-V3",
@@ -40,137 +41,180 @@ namespace Expert_system_for_choosing_antivirus_software
                 "F-Secure",
                 "Fortinet",
                 "GData",
-                "Gridinsoft (no cloud)"
+                "Gridinsoft (no cloud)",
+                "Ikarus",
+                "Jiangmin",
+                "K7AntiVirus",
+                "K7GW",
+                "Kaspersky",
+                "Kingsoft",
+                "Lionic",
+                "Malwarebytes",
+                "MAX",
+                "MaxSecure",
+                "McAfee",
+                "McAfee-GW-Edition",
+                "Microsoft",
+                "NANO-Antivirus",
+                "Palo Alto Networks",
+                "QuickHeal",
+                "Rising",
+                "Sangfor Engine Zero",
+                "SecureAge",
+                "SentinelOne (Static ML)"
             };
-            //foreach (var name in namesOfSoft)
-            //    Console.WriteLine(name);
 
-            int security;
-            int ease_of_use;
-            int comprehensive_protection_availability;
-            int protection_quality;
-            int efficiency_of_threat_detection;
-            int memory_footprint;
-            int update_frequency;
-            int infected_computer_curing;
-            int performance_impact;
-            int virus_database_updates_frequency;
+
+            int firstChoise;
+            int secondChoice;
+            int thirdChoice;
 
             bool result;
 
-            Console.WriteLine("Далее Вам будут заданы вопросы для подбора соответствующего Вам антивирусного пакета. Ответьте на них, используя цифры 1 или 2 (ДА или НЕТ соответственно)\r\n");
-            
-            Console.WriteLine("Вам важна надежность антивирусного пакета?");
+            Console.WriteLine("Далее Вам будут заданы вопросы для подбора соответствующего Вам антивирусного пакета. Ответьте на них, используя соответствующие цифры\r\n");
+
+            Console.WriteLine("Какой тип антивирусного ПО вам требуется? 1-Антивирусный сканер, 2-Антивирусный сторож, 3-Полифаг, 4-Ревизор, 5-Блокировщик");
             do
             {
-                result = int.TryParse(Console.ReadLine(), out security);
-            } while (!result || security < 1 || security > 2);
-            Console.WriteLine(security);//first decision
+                result = int.TryParse(Console.ReadLine(), out firstChoise);
+            } while (!result || firstChoise < 1 || firstChoise > 5);
+            //Console.WriteLine(firstChoise);
 
-            if (security == 1)//vetka 1
+            Console.WriteLine("Вам необходим платный продукт? 1-Платный, 2-Бесплатный");
+            do
             {
-                Console.WriteLine("Важно ли удобство использования антивирусного пакета");
-                do
-                {
-                    result = int.TryParse(Console.ReadLine(), out ease_of_use);
-                } while (!result || ease_of_use < 1 || ease_of_use > 2);
-                Console.WriteLine(ease_of_use);
-                
-                if (ease_of_use == 1)
-                Console.WriteLine("Вам  важна ");
-            }
+                result = int.TryParse(Console.ReadLine(), out secondChoice);
+            } while (!result || secondChoice < 1 || secondChoice > 2);
+            //Console.WriteLine(secondChoice);
 
-
-            else if (security == 2)//vetka 2
+            Console.WriteLine("Выберите операционную систему, на которую собираетесь устанавливать продукт. 1-Windows, 2-Linux, 3-Mac, 4-Android, 5-IOS");
+            do
             {
-                Console.WriteLine("Вам важно наличие комплексной защиты?");
-                do
-                {
-                    result = int.TryParse(Console.ReadLine(), out comprehensive_protection_availability);
-                } while (!result || comprehensive_protection_availability < 1 || comprehensive_protection_availability > 2);
-                Console.WriteLine(comprehensive_protection_availability);
-                if(comprehensive_protection_availability == 1)
-                {
-                    Console.WriteLine(comprehensive_protection_availability);
-                }
-            }
+                result = int.TryParse(Console.ReadLine(), out thirdChoice);
+            } while (!result || thirdChoice < 1 || thirdChoice > 5);
+            //Console.WriteLine(thirdChoice);
 
+            Console.WriteLine("Рекомендованное решение:");
 
+            //Условия для 1-1-х
+            if (firstChoise == 1 && secondChoice == 1 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[1]);
+            if (firstChoise == 1 && secondChoice == 1 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[2]);
+            if (firstChoise == 1 && secondChoice == 1 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[3]);
+            if (firstChoise == 1 && secondChoice == 1 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[4]);
+            if (firstChoise == 1 && secondChoice == 1 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[5]);
 
+            //Условия для 1-2-х
+            if (firstChoise == 1 && secondChoice == 2 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[6]);
+            if (firstChoise == 1 && secondChoice == 2 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[7]);
+            if (firstChoise == 1 && secondChoice == 2 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[8]);
+            if (firstChoise == 1 && secondChoice == 2 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[9]);
+            if (firstChoise == 1 && secondChoice == 2 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[10]);
 
+            //Условия для 2-1-х
+            if (firstChoise == 2 && secondChoice == 1 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[11]);
+            if (firstChoise == 2 && secondChoice == 1 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[12]);
+            if (firstChoise == 2 && secondChoice == 1 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[13]);
+            if (firstChoise == 2 && secondChoice == 1 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[14]);
+            if (firstChoise == 2 && secondChoice == 1 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[15]);
+            
+            //Условия для 2-2-х
+            if (firstChoise == 2 && secondChoice == 2 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[16]);
+            if (firstChoise == 2 && secondChoice == 2 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[17]);
+            if (firstChoise == 2 && secondChoice == 2 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[18]);
+            if (firstChoise == 2 && secondChoice == 2 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[19]);
+            if (firstChoise == 2 && secondChoice == 2 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[20]);
 
+            //Условия для 3-1-х
+            if (firstChoise == 3 && secondChoice == 1 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[21]);
+            if (firstChoise == 3 && secondChoice == 1 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[22]);
+            if (firstChoise == 3 && secondChoice == 1 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[23]);
+            if (firstChoise == 3 && secondChoice == 1 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[24]);
+            if (firstChoise == 3 && secondChoice == 1 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[25]);
 
+            //Условия для 3-2-х
+            if (firstChoise == 3 && secondChoice == 2 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[26]);
+            if (firstChoise == 3 && secondChoice == 2 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[27]);
+            if (firstChoise == 3 && secondChoice == 2 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[28]);
+            if (firstChoise == 3 && secondChoice == 2 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[29]);
+            if (firstChoise == 3 && secondChoice == 2 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[30]);
 
+            //Условия для 4-1-х
+            if (firstChoise == 4 && secondChoice == 1 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[31]);
+            if (firstChoise == 4 && secondChoice == 1 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[32]);
+            if (firstChoise == 4 && secondChoice == 1 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[33]);
+            if (firstChoise == 4 && secondChoice == 1 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[34]);
+            if (firstChoise == 4 && secondChoice == 1 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[35]);
 
+            //Условия для 4-2-х
+            if (firstChoise == 4 && secondChoice == 2 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[36]);
+            if (firstChoise == 4 && secondChoice == 2 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[37]);
+            if (firstChoise == 4 && secondChoice == 2 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[38]);
+            if (firstChoise == 4 && secondChoice == 2 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[39]);
+            if (firstChoise == 4 && secondChoice == 2 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[40]);
 
+            //Условия для 5-1-х
+            if (firstChoise == 5 && secondChoice == 1 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[41]);
+            if (firstChoise == 5 && secondChoice == 1 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[42]);
+            if (firstChoise == 5 && secondChoice == 1 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[43]);
+            if (firstChoise == 5 && secondChoice == 1 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[44]);
+            if (firstChoise == 5 && secondChoice == 1 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[45]);
 
-
-
-
-            //Console.WriteLine("Вам важно удобство использования антивирусного пакета?");
-            //do
-            //{
-            //    result = int.TryParse(Console.ReadLine(), out ease_of_use);
-            //} while (!result || ease_of_use < 1 || ease_of_use > 2);
-            //Console.WriteLine(ease_of_use);
-
-            //Console.WriteLine("Вам важно наличие комплексной защиты?");
-            //do
-            //{
-            //    result = int.TryParse(Console.ReadLine(), out comprehensive_protection_availability);
-            //} while (!result || comprehensive_protection_availability < 1 || comprehensive_protection_availability > 2);
-            //Console.WriteLine(comprehensive_protection_availability);
-
-            //Console.WriteLine("Насколько Вам важно качество защиты?");
-            //do
-            //{
-            //    result = int.TryParse(Console.ReadLine(), out protection_quality);
-            //} while (!result || protection_quality < 1 || protection_quality > 2);
-            //Console.WriteLine(protection_quality);
-
-            //Console.WriteLine("Насколько Вам важна высокая эффективность обнаружения угроз?");
-            //do
-            //{
-            //    result = int.TryParse(Console.ReadLine(), out efficiency_of_threat_detection);
-            //} while (!result || efficiency_of_threat_detection < 1 || efficiency_of_threat_detection > 2);
-            //Console.WriteLine(efficiency_of_threat_detection);
-
-            //Console.WriteLine("Насколько Вам важен обьем занимаемой антивирусным пакетом памяти?");
-            //do
-            //{
-            //    result = int.TryParse(Console.ReadLine(), out memory_footprint);
-            //} while (!result || memory_footprint < 1 || memory_footprint > 2);
-            //Console.WriteLine(memory_footprint);
-
-            //Console.WriteLine("Насколько Вам важна высокая частота получения обновлений?");
-            //do
-            //{
-            //    result = int.TryParse(Console.ReadLine(), out update_frequency);
-            //} while (!result || update_frequency < 1 || update_frequency > 2);
-            //Console.WriteLine(update_frequency);
-
-            //Console.WriteLine("Насколько Вам важна возможность вылечить зараженый компьютер?");
-            //do
-            //{
-            //    result = int.TryParse(Console.ReadLine(), out infected_computer_curing);
-            //} while (!result || infected_computer_curing < 1 || infected_computer_curing > 2);
-            //Console.WriteLine(infected_computer_curing);
-
-            //Console.WriteLine("Насколько Вам важно низкое влияние на производительность?");
-            //do
-            //{
-            //    result = int.TryParse(Console.ReadLine(), out performance_impact);
-            //} while (!result || performance_impact < 1 || performance_impact > 2);
-            //Console.WriteLine(performance_impact);
-
-            //Console.WriteLine("Насколько Вам важна высокая периодичность обновления вирусных баз?");
-            //do
-            //{
-            //    result = int.TryParse(Console.ReadLine(), out virus_database_updates_frequency);
-            //} while (!result || virus_database_updates_frequency < 1 || virus_database_updates_frequency > 2);
-            //Console.WriteLine(virus_database_updates_frequency);
-
-
+            //Условия для 5-2-х
+            if (firstChoise == 5 && secondChoice == 2 && thirdChoice == 1)
+                Console.WriteLine(knowledgeBase[46]);
+            if (firstChoise == 5 && secondChoice == 2 && thirdChoice == 2)
+                Console.WriteLine(knowledgeBase[47]);
+            if (firstChoise == 5 && secondChoice == 2 && thirdChoice == 3)
+                Console.WriteLine(knowledgeBase[48]);
+            if (firstChoise == 5 && secondChoice == 2 && thirdChoice == 4)
+                Console.WriteLine(knowledgeBase[49]);
+            if (firstChoise == 5 && secondChoice == 2 && thirdChoice == 5)
+                Console.WriteLine(knowledgeBase[50]);
 
 
 
